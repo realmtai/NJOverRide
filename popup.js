745cccb147fec79a28c74435d85528a6c8d43bcd
yong <yong.dai@nexj.com>
2013-07-05 15:33:00 -0400

    injection is working.

--- popup.js
+++ popup.js
@@ -2,22 +2,22 @@
 
 
 
-// if(!document.head){ setTimeout(insertScript, 1); }
-// else { insertScript(); }
+// if(!document.head){ setTimeout(insertJavaScript, 1); }
+// else { insertJavaScript(); }
 
-// function insertScript(){
+// function insertJavaScript(){
 //   injectScript("inj.js");
 // }
-
-function main(scripts){
-  var w = window;
-  w.alert = function (msg){
-    console.log(msg);
+function insertJavaScript(jsFile){
+    var head = document.getElementsByTagName('head')[0] || document.head;
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = jsFile;
+    console.log(jsFile);
+    if(head) head.insertBefore(script, head.firstChild);
   };
-}
 
-
-function insertScript(jsFile){
+  function insertCSSScript(jsFile){
     var head = document.getElementsByTagName('head')[0] || document.head;
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -27,19 +27,40 @@ function insertScript(jsFile){
   };
 
 
+
+function main(args){
+  var w = window;
+  w.alert = function (msg){
+    console.log(msg);
+  };
+}
+
+
+
+
+
+
+
+
+
+
 if (!document.xmlVersion) {
   var script = document.createElement('script');
-  var scripts=[chrome.extension.getURL("inj.js")];
+  
 
-
-
-  // var scrArgs = scripts.toString().split(",").join("\",\"");
-  script.appendChild(document.createTextNode('('+ main +')();'));
-
-
-  for (var i = 0; i < scripts.length; i++) {
-    insertScript(scripts[i]);
-  };
-
+  var scripts=["passed in javascript html page"];
+  var scrArgs = scripts.toString().split(",").join("\",\"");
+  script.appendChild(document.createTextNode('('+ main +')([\"'+scrArgs+'\"]);'));
   document.documentElement.appendChild(script);
+
+
+  var insertJavaScriptHtml = function (){
+    insertJavaScript(chrome.extension.getURL("inj.js"));
+    insertJavaScript(chrome.extension.getURL("jquery.js"));
+  }
+
+  if(!document.head){ setTimeout(insertJavaScriptHtml, 1); }
+  else {insertJavaScriptHtml();}
+    
+
 }
